d30511c5914131e35aab3aa5da9ef06e0562a746
raulh39 <raulh39@gmail.com>
2021-08-22 11:47:20 +0200

    Create a main() skeleton

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,19 @@
 #include <functional>
 #include <iostream>
 
-#include <spdlog/spdlog.h>
 #include <docopt/docopt.h>
+#include <fmt/core.h>
 
 static constexpr auto USAGE =
-  R"(Naval Fate.
+  R"(Simple SVG Floorplanner.
 
     Usage:
-          naval_fate ship new <name>...
-          naval_fate ship <name> move <x> <y> [--speed=<kn>]
-          naval_fate ship shoot <x> <y>
-          naval_fate mine (set|remove) <x> <y> [--moored | --drifting]
-          naval_fate (-h | --help)
-          naval_fate --version
+          simple_svg_floorplan <input_file>
+          simple_svg_floorplan (-h | --help)
+          simple_svg_floorplan --version
  Options:
           -h --help     Show this screen.
           --version     Show version.
-          --speed=<kn>  Speed in knots [default: 10].
-          --moored      Moored (anchored) mine.
-          --drifting    Drifting mine.
 )";
 
 int main(int argc, const char **argv)
@@ -27,15 +21,7 @@ int main(int argc, const char **argv)
   std::map<std::string, docopt::value> args = docopt::docopt(USAGE,
     { std::next(argv), std::next(argv, argc) },
     true,// show help if requested
-    "Naval Fate 2.0");// version string
+    "Simple SVG Floorplanner 0.1");// version string
 
-  for (auto const &arg : args) {
-    std::cout << arg.first << arg.second << std::endl;
-  }
-
-
-  //Use the default logger (stdout, multi-threaded, colored)
-  spdlog::info("Hello, {}!", "World");
-
-  fmt::print("Hello, from {}\n", "{fmt}");
+  fmt::print("Nothing to see here. Keep going. Keep the good work with \"{}\"\n", args.at("<input_file>").asString());
 }
