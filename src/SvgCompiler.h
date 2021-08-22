a32a00f5fcd42c531f101a58e9877421c71163b2
raulh39 <raulh39@gmail.com>
2021-08-22 13:41:23 +0200

    Using an OBJECT lib to develop tests

---
+++ src/SvgCompiler.h
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace simple_svg_floorplan
+{
+  class SvgCompiler {};
+}
--- test/tests.cpp
+++ test/tests.cpp
@@ -1,14 +1,8 @@
+#include "SvgCompiler.h"
+
 #include <catch2/catch.hpp>
 
-unsigned int Factorial(unsigned int number)
+TEST_CASE("starting")
 {
-  return number <= 1 ? number : Factorial(number - 1) * number;
-}
-
-TEST_CASE("Factorials are computed", "[factorial]")
-{
-  REQUIRE(Factorial(1) == 1);
-  REQUIRE(Factorial(2) == 2);
-  REQUIRE(Factorial(3) == 6);
-  REQUIRE(Factorial(10) == 3628800);
+  simple_svg_floorplan::SvgCompiler compiler;
 }
