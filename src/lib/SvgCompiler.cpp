c6e1a56e9ee4394fcad159d3966bf7a5856cc261
raulh39 <raulh39@gmail.com>
2021-08-27 19:00:15 +0200

    Starting tests with the compiler

--- src/lib/SvgCompiler.cpp
+++ src/lib/SvgCompiler.cpp
@@ -2,6 +2,9 @@
 
 using namespace simple_svg_floorplan;
 
-void SvgCompiler::Compile(std::istream &, std::ostream &)
+void SvgCompiler::Compile(std::istream &, std::ostream &os)
 {
+  os << R"(<svg xmlns="http://www.w3.org/2000/svg">)";
+  os << '\n';
+  os << "</svg>";
 }
--- test/tests.cpp
+++ test/tests.cpp
@@ -3,10 +3,11 @@
 #include <catch2/catch.hpp>
 #include <sstream>
 
-TEST_CASE("starting")
+TEST_CASE("Empty input produces the minimal SVG")
 {
   simple_svg_floorplan::SvgCompiler compiler;
   std::istringstream input{ "" };
   std::ostringstream output;
   compiler.Compile(input, output);
+  REQUIRE(output.str() == R"(<svg xmlns="http://www.w3.org/2000/svg">)""\n</svg>");
 }
